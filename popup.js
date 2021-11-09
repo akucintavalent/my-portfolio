b4035ddaec02c22c53491b562b94c55e77e7449b
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2021-11-09 10:21:57 +0100

    Fix linter errors

--- popup.js
+++ popup.js
@@ -4,20 +4,20 @@ const closeBtn = document.querySelector('.close-btn');
 const links = document.querySelectorAll('.nav-mobile-modal a');
 const openBtn = document.querySelector('i.fa-bars');
 
-function open () {
+function open() {
   popup.classList.toggle('invisible');
   body.style.overflow = 'hidden';
 }
 
-function close () {
+function close() {
   popup.classList.toggle('invisible');
   body.style.overflow = 'auto';
 }
 
 openBtn.addEventListener('click', open);
-  
+
 closeBtn.addEventListener('click', close);
-  
+
 links.forEach((link) => {
   link.addEventListener('click', close);
 });
