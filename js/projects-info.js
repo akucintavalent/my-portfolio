b5d1505a814fea799f8bf417c122d3f0b78ad4b9
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-05-25 16:10:59 +0200

    Remove not very cool projects

--- js/projects-info.js
+++ js/projects-info.js
@@ -1,43 +1,6 @@
 export default
 [
   {
-    title: 'My portfolio',
-    imageSrc: './images/projects/project_card_1.png',
-    tags: [
-      'HTML',
-      'CSS',
-      'JavaScript',
-    ],
-    text: 'This project is actually the cite you are reading this text at.',
-    liveLink: 'https://akucintavalent.github.io/my-portfolio/',
-    sourceLink: 'https://github.com/akucintavalent/my-portfolio',
-  },
-  {
-    title: 'Microverse capstone project',
-    imageSrc: './images/projects/project_card_2.png',
-    tags: [
-      'HTML',
-      'CSS',
-      'JavaScript',
-    ],
-    text: 'This is a capstone project with mock data and pictures.',
-    liveLink: 'https://akucintavalent.github.io/capstone-project-1/',
-    sourceLink: 'https://github.com/akucintavalent/capstone-project-1',
-  },
-  {
-    title: 'To do list',
-    imageSrc: './images/projects/project_card_3.png',
-    tags: [
-      'HTML',
-      'Webpack',
-      'CSS',
-      'JavaScript',
-    ],
-    text: 'This is a simple task list.',
-    liveLink: 'https://akucintavalent.github.io/to-do-list/',
-    sourceLink: 'https://github.com/akucintavalent/to-do-list',
-  },
-  {
     title: 'Movies',
     imageSrc: './images/projects/project_card_4.png',
     tags: [
@@ -49,33 +12,6 @@ export default
     text: 'In this JavaScript capstone project we built a web application based on an external API. The webapp has 2 user interfaces:\n- A home page showing a list of items (movies) that you can "like."\n- A popup window with more data about an item that you can use to comment on it.',
     liveLink: null,
     sourceLink: 'https://github.com/akucintavalent/js-capstone',
-  },
-  {
-    title: 'Math magicians',
-    imageSrc: './images/projects/project_card_5.png',
-    tags: [
-      'React',
-      'JavaScript',
-      'CSS',
-      'HTML',
-    ],
-    text: 'Math magicians is an app that contains a calculator built to calculate simple mathematical operations.',
-    liveLink: 'https://math-magicians-akucintavalent.herokuapp.com',
-    sourceLink: 'https://github.com/akucintavalent/math-magicians',
-  },
-  {
-    title: 'Bookstore',
-    imageSrc: './images/projects/project_card_6.png',
-    tags: [
-      'React',
-      'Redux',
-      'JavaScript',
-      'CSS',
-      'Webpack',
-    ],
-    text: 'In this web app you can: display a list of books, add a book, and remove a selected book.',
-    liveLink: 'https://bookstore-akucintavalent.herokuapp.com/',
-    sourceLink: 'https://github.com/akucintavalent/bookstore',
   },
   {
     title: 'Space Travelers\' Hub',
@@ -130,19 +66,6 @@ export default
     text: 'The Recipe app keeps track of all the users\' recipes, ingredients, and inventory. It will allow the user to save ingredients, keep track of what they have, create recipes, and generate a shopping list based on what they have and what they are missing from a recipe. Also, since sharing recipes is an important part of cooking the app should allow the user to make them public so anyone can access them.',
     liveLink: null,
     sourceLink: 'https://github.com/Meri-MG/recipe-app',
-  },
-  {
-    title: 'Budget App',
-    imageSrc: './images/projects/project_card_11.png',
-    tags: [
-      'Ruby on Rails',
-      'CSS',
-      'HTML',
-      'PostgreSQL',
-    ],
-    text: 'The Ruby on Rails Budget App is about building a mobile web application where user can manage the budget: they have a list of transactions associated with a category, so that the user can see how much money they spent and on what.',
-    liveLink: 'https://stormy-journey-61198.herokuapp.com/',
-    sourceLink: 'https://github.com/akucintavalent/budget',
   },
   {
     title: 'Car Booking Service',
